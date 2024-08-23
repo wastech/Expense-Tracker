b9f2ca35da22ee7f9feee8c70de7a9496a46d82f
wastech <fataiwasiu2@gmail.com>
2024-08-23 22:49:47 +0100

    setup the project :art:

---
+++ src/test/java/com/wastech/Expense_Tracker/ExpenseTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wastech.Expense_Tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
